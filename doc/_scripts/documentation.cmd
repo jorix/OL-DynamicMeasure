f56b6be243a26e27b671294203ab5a6885dafa93
Xavier Mamano <xavier.mamano@gmail.com>
2013-05-15 17:14:35 +0200

    Custom CSS for docs

--- doc/_scripts/documentation.cmd
+++ doc/_scripts/documentation.cmd
@@ -1,4 +1,4 @@
-call naturaldocs -i ../../lib/OpenLayers/Control -hl All -s Default OL -o HTML ../DynamicMeasure/all -p ../DynamicMeasure/all/_config
-call naturaldocs -i ../../lib/OpenLayers/Control -hl All -s Default OL -o HTML ../DynamicMeasure/api -p ../DynamicMeasure/api/_config
+call naturaldocs -i ../../lib/OpenLayers/Control -hl All -s Default ../../../_scripts/jorix -o HTML ../DynamicMeasure/all -p ../DynamicMeasure/all/_config
+call naturaldocs -i ../../lib/OpenLayers/Control -hl All -s Default ../../../_scripts/jorix -o HTML ../DynamicMeasure/api -p ../DynamicMeasure/api/_config
 
 pause
