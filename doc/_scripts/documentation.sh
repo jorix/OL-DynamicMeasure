9dd865724ca35908b81666c214a94c7914f87f7c
Jean-Denis Giguère <jdenisgiguere@gmail.com>
2016-03-16 08:35:38 -0400

    Build documentation from bash shell

---
+++ doc/_scripts/documentation.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+naturaldocs -i ../../lib/OpenLayers/Control -hl All -s Default ../../../_scripts/jorix -o HTML ../DynamicMeasure/all -p ../DynamicMeasure/all/_config
+naturaldocs -i ../../lib/OpenLayers/Control -hl All -s Default ../../../_scripts/jorix -o HTML ../DynamicMeasure/api -p ../DynamicMeasure/api/_config
